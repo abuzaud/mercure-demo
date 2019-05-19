1177b53d5cd6d7331fb0ad7e3ec9530043fed986
Antoine Buzaud <antoine.buzuad@gmail.com>
2019-05-19 17:46:48 +0200

    Installation de symfony

---
+++ assets/js/layout.js
@@ -0,0 +1,7 @@
+// Global npm Dependencies
+import 'popper.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// const bootstrap = require('bootstrap/dist/js/bootstrap.min');
+
+// Global local dependencies
+require('../scss/layout.scss');
---
+++ assets/js/mercure-demo.js
@@ -0,0 +1,9 @@
+// Global npm dependencies
+const $ = require('jquery');
+
+// Local dependencies
+require('../scss/mercure-demo.scss');
+
+$(document).ready(() => {
+    console.log('mercure loaded');
+});

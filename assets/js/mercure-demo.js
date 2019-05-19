0c0b3f55123f3bc6d9b73a62c40a41fb62ed6b84
Antoine Buzaud <antoine.buzuad@gmail.com>
2019-05-19 22:35:04 +0200

    Installation de mercure

--- assets/js/mercure-demo.js
+++ assets/js/mercure-demo.js
@@ -6,4 +6,7 @@ require('../scss/mercure-demo.scss');
 
 $(document).ready(() => {
     console.log('mercure loaded');
+    const url = new URL('https://mercure-demo.buzaud.com/subscribe');
+    // url.searchParams.append('topic', 'https://example.com/books/{id}');
+    // url.searchParams.append('topic', 'https://example.com/users/dunglas');
 });
---
+++ start_mercure.sh
@@ -0,0 +1,2 @@
+ #!/bin/bash
+  JWT_KEY='aVerySecretKey' ADDR='localhost:3000' ALLOW_ANONYMOUS=1 CORS_ALLOWED_ORIGINS=* ./mercure/mercure
